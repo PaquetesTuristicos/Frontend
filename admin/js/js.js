cb8b792d1b6d346afbbdce95cc2703aa40b3425f
juancapo2010 <juancapo2010@gmail.com>
2021-07-02 20:57:50 -0300

    usuario

--- admin/js/js.js
+++ admin/js/js.js
@@ -1,5 +1,3 @@
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjciLCJuYmYiOjE2MjQ1Nzg3MTUsImV4cCI6MTYyNTE4MzUxNSwiaWF0IjoxNjI0NTc4NzE1fQ.AuiMP4Pr9gElhDbSPvwb2EcBQX-FH0onLpQ2QYHaKkU'
-
 var pagina = "http://localhost/paquetes/Frontend/admin/"
 
 var bottomPanel = document.getElementById('bottomPanel');
@@ -9,18 +7,22 @@ var bottomViajes = document.getElementById('bottomViajes');
 var bottomTransporte = document.getElementById('bottomTransporte');
 var bottomUsuario = document.getElementById('bottomUsuario');
 
-// active 
-function buttomActive(){
-    
-}
-
 // cabecera 
 myHeaders = new Headers({
     'Accept': 'application/json',
     'Content-Type': 'application/json',
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJuYmYiOjE2MjQ2NjY2MTMsImV4cCI6MTYyNTI3MTQxMywiaWF0IjoxNjI0NjY2NjEzfQ.XgAwPjkC-1A2lW_d_pPAKMZQtPaPXIpw-3lMV0Bp0_U",
+    "Authorization": "Bearer "+localStorage.getItem('token') ,
     credentials: 'include',
   });
+// active 
+function buttomActive(){
+    
+}
+function salir(){
+    localStorage.clear()
+    window.location.href = "../login.html";
+}
+
 // usuario 
 bottomUsuario.addEventListener('click', function (e) {
     e.preventDefault()
@@ -51,3 +53,6 @@ bottomPanel.addEventListener('click', function (e) {
     e.preventDefault()
     location.assign(pagina + "panel/");
 })
+
+var empleado = document.querySelector('#empleado')
+empleado.append(localStorage.getItem('nombre'))
