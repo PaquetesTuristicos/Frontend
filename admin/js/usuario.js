23ce2330ed2e733f2c1f4a722df6a3abd63b2c43
juancapo2010 <juancapo2010@gmail.com>
2021-07-06 20:03:26 -0300

    pasajero empleado

--- admin/js/usuario.js
+++ admin/js/usuario.js
@@ -1,11 +1,15 @@
 // Usuario 
-console.log(localStorage.getItem('token'))
+
 
 
 // var bottomUsuario = document.getElementById('bottomUsuario');
 var msusuario = `https://localhost:44384/api/user/`
+var msempleado = `https://localhost:44384/api/empleado/`
+var mspasajero = `https://localhost:44384/api/pasajero/`
 
 var usuario = document.querySelector('#contenido')
+var empleado = document.querySelector('#contenidoEmpleado')
+var pasajero = document.querySelector('#contenidoPasajero')
 pintar()
 function pintar(){
     fetch(msusuario, {
@@ -20,104 +24,8 @@ function pintar(){
 // carga datos en pantalla 
 function usuarios(data) {
     usuario.innerHTML = ''
-    usuario.innerHTML = `<div>
-    <div>
-        <H3>Usuarios</H3>
-    </div>
-</div>
-<div>
-<div>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#cargarUsuarioModal">
-Nuevo Usuario
-</button>   
-<!-- Modal -->
-<div class="modal fade" id="cargarUsuarioModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Nuevo Usuario</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form id="cargarUsuarioForm">                   
-                    <input type="text" name="nombre" placeholder="Ingresa Nombre" class="form-control my-3" required />
-                    <input type="text" name="apellido" placeholder="Ingresa Apellido" class="form-control my-3" required />
-                    <input type="text" name="email" placeholder="Ingresa Email" class="form-control my-3" required />
-                    <input type="password" name="password" placeholder="Ingresa Password" class="form-control my-3" required />
-                    <div class="col-md-4">
-                        <label for="inputState" class="form-label">Tipo</label>
-                        <select id="roll" class="form-select selectRoll" required>
-                            <option selected value="">Seleccione Roll...</option>
-                            <option type="text" value="1" id="1">Administrador</option>
-                            <option type="text" value="2" id="2">Empleado</option>
-                            <option type="text" value="3" id="3">Cliente</option>
-                        </select>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        <button type="button" class="btn btn-primary" onclick="createUsuario()">Crear</button>
-                    </div>
-                </form>
-                <div class="mt-3" id="cargarUsuarioRespuesta">
+    usuario.innerHTML = `
 
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-<!-- Modal editar -->
-<div class="modal fade" id="editarUsuarioModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Editar Usuario</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form id="editUsuarioForm">
-                <input type="number" name="id" placeholder="" class="form-control my-3 id" required />
-                    <input type="text" name="nombre" placeholder="Ingresa Nombre" class="form-control my-3 nombre" required />
-                    <input type="text" name="apellido" placeholder="Ingresa Apellido" class="form-control my-3 apellido" required />
-                    <input type="text" name="email" placeholder="Ingresa Email" class="form-control my-3 email" required />
-                    <input type="password" name="password" placeholder="Ingresa Password" class="form-control my-3 password" required />
-                    <div class="col-md-4">
-                        <label for="inputState" class="form-label">Tipo</label>
-                        <select id="editroll" class="form-select selectRoll" required>
-                            <option selected value="">Seleccione Roll...</option>
-                            <option type="text" value="1" id="1">Administrador</option>
-                            <option type="text" value="2" id="2">Empleado</option>
-                            <option type="text" value="3" id="3">Cliente</option>
-                        </select>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        <button id="editarUsuario" type="button" class="btn btn-primary">Editar</button>
-                    </div>
-                </form>
-                <div class="mt-3" id="editarUsuarioRespuesta">
-
-                </div>
-            </div>
-
-        </div>
-    </div>
-</div>
-        <button type="button" class="btn btn-primary">Buscar Usuario</button>
-      
-        <div class="btn-group" role="group">
-          <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Configuracion
-          </button>
-          <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-            <li><a class="dropdown-item" href="#">nose</a></li>
-            <li><a class="dropdown-item" href="#">definir</a></li>
-          </ul>
-        </div>
-      
-</div>
-</div>
 <div class="row justify-content-center align-items-center">
 <table class="table">
     <thead>
@@ -243,3 +151,107 @@ function deliteUsuario(id){
         location.reload()
     })
 };
+// empleado 
+function empleados() {
+    console.log('empleado')
+    fetch(msempleado, {
+        method: 'GET',
+        headers: myHeaders,
+    })
+        .then(res => res.json())
+        .then(datos => {
+            tablaEmpleado(datos)
+        })
+}
+function tablaEmpleado(datos){
+
+    empleado.innerHTML = `
+    <div class="row">
+    <h3>Empleado</h3>
+      </div>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">DNI</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Cumpleaños</th>
+        <th scope="col">Legajo</th>
+        <th scope="col">Sueldo</th>
+        <th scope="col">Email</th>
+        <th scope="col">accion</th>
+      </tr>
+    </thead>
+    <tbody id="tbodyEmpleado">
+    </tbody>
+    </table>
+</div>`
+var tabla = document.querySelector('#tbodyEmpleado')
+    for (let valor of datos) {
+        tabla.innerHTML += `
+        <tr>
+        <th scope="row">${valor.empleadoId}</th>
+        <td>${valor.dni}</td>
+        <td>${valor.telefono}</td>
+        <td>${valor.fechaNacimiento}</td>
+        <td>${valor.legajo}</td>
+        <td>${valor.sueldo}</td>
+        <td>${valor.user.email}</td>
+        <td>
+            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#editarUsuarioModal" onclick="editEmpleado(${valor.empleadoId})">Editar</button>
+            <button type="button" class="btn btn-outline-danger" onclick="deliteEmpleado(${valor.empleadoId})">Borrar</button>
+        </td>
+      </tr>
+    `
+    }
+}
+// Pasajero 
+function pasajeros() {
+    console.log('empleado')
+    fetch(mspasajero, {
+        method: 'GET',
+        headers: myHeaders,
+    })
+        .then(res => res.json())
+        .then(datos => {
+            tablaPasajero(datos)
+        })
+}
+function tablaPasajero(datos){
+
+    pasajero.innerHTML = `
+    <div class="row">
+    <h3>Pasajero</h3>
+      </div>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">DNI</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Cumpleaños</th>
+        <th scope="col">Email</th>
+        <th scope="col">accion</th>
+      </tr>
+    </thead>
+    <tbody id="tbodyPasajero">
+    </tbody>
+    </table>
+</div>`
+var tabla = document.querySelector('#tbodyPasajero')
+    for (let valor of datos) {
+        tabla.innerHTML += `
+        <tr>
+        <th scope="row">${valor.pasajeroId}</th>
+        <td>${valor.dni}</td>
+        <td>${valor.telefono}</td>
+        <td>${valor.fechaNacimiento}</td>
+        <td>${valor.user.email}</td>
+        <td>
+            <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#editarUsuarioModal" onclick="editEmpleado(${valor.pasajeroId})">Editar</button>
+            <button type="button" class="btn btn-outline-danger" onclick="deliteEmpleado(${valor.pasajeroId})">Borrar</button>
+        </td>
+      </tr>
+    `
+    }
+}
